b46d0eef7e75e31b7978a0d08680a2c0fe00b31f
Patrick Hussey <husspm@gmail.com>
2017-02-14 16:35:28 -0500

    initial tests

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+var word = ["Piano", "Guitar", "Drums", "Bass", "Violin", "Cello", "Tuba", "Saxophone", "Trumpet"];
+console.log(word);
+console.log(word.length);
+function play(){
+var guess = word[Math.floor(Math.random()*word.length)];
+var guess = guess.toLowerCase();
+var letters = guess.split("");
+console.log(letters);
+var blank = " ";
+var space = guess.length * blank.length;
+var i;
+for (i = 0; i < space; i++) {
+blank +="_" + " ";
+document.getElementById('print').innerHTML=(blank);	
+}
+}
+document.onkeyup = function(event){
+	var playerGuess= event.key;
+	var playerGuess=playerGuess.toLowerCase();
+	console.log(playerGuess);
+} 
